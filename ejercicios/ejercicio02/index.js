84335c76bfec34e73cd9f726d5107a22356a3cf3
Tzuzul Code <mail@tzuzulcode.com>
2022-04-22 20:09:01 -0500

    Drag and drop

--- ejercicios/ejercicio02/index.js
+++ ejercicios/ejercicio02/index.js
@@ -1,5 +1,8 @@
 const dropzone = document.getElementById("dropzone")
+const dropzone2 = document.getElementById("dropzone2")
 const texto = document.getElementById("texto")
+
+let element = null
 
 dropzone.ondragover = (event)=>{
     event.preventDefault()
@@ -13,13 +16,50 @@ dropzone.ondragleave=()=>{
 dropzone.ondrop = ()=>{
     dropzone.classList.add("dropped")
 
+    dropzone.appendChild(element)
+    texto.style.visibility="visible"
+
     setTimeout(()=>{
         dropzone.classList.remove("dropped")
     },1000)
 }
 
+dropzone2.ondragover = (event)=>{
+    event.preventDefault()
+}
+
+dropzone2.ondrop = (event)=>{
+    dropzone2.appendChild(element)
+    texto.style.visibility=true
+}
+
+texto.ondragstart = (event)=>{
+    console.log()
+    element = texto
+    texto.style.visibility = false
+}
+
+// texto.ondrag = (event)=>{
+//     event.preventDefault()
+    
+//     texto.style.position = "absolute"
+//     texto.style.left = event.pageX+'px'
+//     texto.style.top = event.pageY+'px'
+// }
+
+// texto.ondragend = (event)=>{
+//     texto.style.visibility="visible"
+// }
 
 texto.ontouchmove = (event)=>{
-    console.log(event)
-    alert("Hola")
+    let touchLocation = event.targetTouches[0]
+
+    texto.style.position = "absolute"
+    texto.style.left = touchLocation.pageX+'px'
+    texto.style.top = touchLocation.pageY+'px'
+
+}
+
+texto.ontouchend = (event)=>{
+
 }

96fa592788860f0f1f940762e5fa8f03a6f3d0dd
Tzuzul <mail@tzuzulcode.com>
2022-05-31 19:39:10 -0500

    svg

--- temario/animations and events/index.js
+++ temario/animations and events/index.js
@@ -6,7 +6,18 @@ button.addEventListener("click",()=>{
     notification.classList.add("transparent")
 })
 
+button.addEventListener("animationstart",()=>{
+    console.log("Animation started")
+})
+button.addEventListener("animationiteration",()=>{
+    console.log("Animation iteration")
+})
+button.addEventListener("animationend",()=>{
+    console.log("Animation ended")
+})
+
 
 notification.addEventListener("transitionend",()=>{
     notification.style.display = "none"
+    // button.style.animation = ""
 })

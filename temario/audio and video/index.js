bda7a872ea4ef8adb1faacbf95518c708918a1b2
Tzuzul <mail@tzuzulcode.com>
2022-06-03 20:10:44 -0500

    Reproductor

--- temario/audio and video/index.js
+++ temario/audio and video/index.js
@@ -2,17 +2,45 @@ const video = document.getElementById("video")
 const audio = document.getElementById("audio")
 const play = document.getElementById("play")
 const control = document.getElementById("control")
+const backwardSeconds = document.getElementById("backwardSeconds")
+const forwardSeconds = document.getElementById("forwardSeconds")
+const fullScreen = document.getElementById("fullScreen")
+
+let duration
 
 video.removeAttribute("controls")
 
 video.addEventListener("playing",()=>{
-    alert("Playing...")
+    
 })
 video.addEventListener("pause",()=>{
-    alert("Pause")
+    
 })
 
+video.addEventListener("loadeddata",(event)=>{
+    duration = event.target.duration
+})
 
+video.addEventListener("timeupdate",(event)=>{
+    const percentage = (event.target.currentTime/duration)*100
+    control.value = percentage
+})
+
+// on input se activa en cada step del range
+control.oninput = (event)=>{
+    video.currentTime = (duration/100)*event.target.value
+}
+
+backwardSeconds.onclick = () =>{
+    video.currentTime = video.currentTime-1
+}
+forwardSeconds.onclick = () =>{
+    video.currentTime = video.currentTime+1
+}
+
+fullScreen.onclick = ()=>{
+    video.requestFullscreen()
+}
 
 play.onclick = ()=>{
     if(video.paused){
@@ -20,9 +48,4 @@ play.onclick = ()=>{
     }else{
         video.pause()
     }
-}
-
-control.onchange = (event)=>{
-    console.log(event.target.value)
-    alert("Changing...")
 }

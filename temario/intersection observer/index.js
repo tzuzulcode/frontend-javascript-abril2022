a58cb88a3532f817ec426fdbf797fd9f4c88aae0
Tzuzul <mail@tzuzulcode.com>
2022-06-09 20:05:04 -0500

    Intersection observer

--- temario/intersection observer/index.js
+++ temario/intersection observer/index.js
@@ -1,10 +1,15 @@
 let documentHeight = document.documentElement.offsetHeight
 let viewportHeight = window.innerHeight
 
-let scrollButton = document.getElementById("scroll")
+let goUpBtn = document.getElementById("goUpBtn")
+let lastCard = document.getElementById("last")
 
 console.log(documentHeight)
 console.log(viewportHeight)
+
+if(window.IntersectionObserver){
+    alert("Intersection observer available")
+}
 
 // scrollTo()
 // scrollTo(x,y)
@@ -24,7 +29,41 @@ const interval = setInterval(()=>{
         behavior:"smooth"
     })
     card++
-    if(card===cardsNumber){
+    if(card>=cardsNumber){
         clearInterval(interval)
     }
 },2000)
+
+goUpBtn.onclick = ()=>{
+    scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
+    })
+}
+
+function onIntersection(changes){
+    console.log(changes)
+    for(let change of changes){
+        if(change.isIntersecting){
+            goUpBtn.style.display = "block"
+        }else{
+            goUpBtn.style.display = "none"
+        }
+    }
+}
+
+const observer = new IntersectionObserver(onIntersection,{
+    // root, //ELemento usado como viewport
+    // rootMargin,
+    threshold:[0.5,1],
+    // trackVisibility,
+    // delay
+})
+
+observer.observe(lastCard)
+
+
+// Lazy loading de imagenes
+// Controlar contenido de algo
+// Infinite scrolling

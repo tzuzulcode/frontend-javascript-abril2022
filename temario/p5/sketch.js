161889c4ded27e30d1b05f1d66a3ab1b6253abe8
Tzuzul <mail@tzuzulcode.com>
2022-06-15 20:19:38 -0500

    P5

---
+++ temario/p5/sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(1920,1080)
+}
+
+function draw(){
+    background(204)
+    // ellipse(50,50,80,80) // (left,top,width,height)
+    // if(mouseIsPressed){
+    //     fill(0)
+    // }else{
+    //     fill(255)
+    // }
+    // ellipse(mouseX,mouseY,80,80)
+    // point(240,60)
+    line(20,50,420,110)
+    triangle(10,10,300,300,150,300)
+    rect(10,10,100,10,100,110,100,110)
+}
